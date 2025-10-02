bbb1daa975f7777a79741320637f62089a09ac26
GauravJ13 <gaurav24221@iiitd.ac.in>
2025-10-02 15:34:05 +0530

    try

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,9 @@ public class Main {
                 // The application now starts with the landing page.
                 LandingFrame landingFrame = new LandingFrame();
                 landingFrame.setVisible(true);
+
+
+
             }
         });
     }
