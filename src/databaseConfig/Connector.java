d38a9076adc34772d7ea8d21ab264355884e5206
Nikhil <nikhilagrawal6448@gmail.com>
2025-10-02 18:55:14 +0530

    update to connection

--- src/databaseConfig/Connector.java
+++ src/databaseConfig/Connector.java
@@ -3,9 +3,9 @@ package databaseConfig;
 import java.sql.*;
 
 public class Connector {
-    private static String url = "jdbc:mysql://localhost:3306/Auth";
-    private static String user = "root";
-    private static String password = "Nikhil@123";
+    private final static String url = "jdbc:mysql://localhost:3306/Auth";
+    private final static String user = "root";
+    private final static String password = "Nikhil@123";
 
     public void connector(){
         System.out.println("Trying to connect to database...");
