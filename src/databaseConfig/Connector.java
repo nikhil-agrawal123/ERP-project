f22bbed06e23a31240c6b8e7f1d6d780a7128f2e
GauravJ13 <gaurav24221@iiitd.ac.in>
2025-10-04 11:47:30 +0530

    Dashboards initiated

--- src/databaseConfig/Connector.java
+++ src/databaseConfig/Connector.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Connector {
     private final static String url = "jdbc:mysql://localhost:3306/Auth";
     private final static String user = "root";
-    private final static String password = "Nikhil@123";
+    private final static String password = "password";
 
     /**
      * Creates and returns a new database connection.
