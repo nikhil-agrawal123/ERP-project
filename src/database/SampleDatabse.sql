1e874758c44c2c160c1892e2f6233bec487533ef
GauravJ13 <gaurav24221@iiitd.ac.in>
2025-10-05 02:15:22 +0530

    parent and admin database update

--- src/database/SampleDatabse.sql
+++ src/database/SampleDatabse.sql
@@ -20,3 +20,23 @@ values ("nikhil","nikhil"),
 update auth.facultyauth
 set facultyPass = "$2a$10$Kc/0I11BVSsNDxsuYAsJDekSvfAze0t7auWjff5OF6oC/anfVdVje"
 where id = 1;
+
+
+insert into adminAuth(adminId,adminName, adminPass)
+values ("1","nikhil","nikhil"),
+       ("2","gaurav", "gaurav"),
+       ("3","aditri", "aditri");
+
+update auth.adminAuth
+set adminPass = "$2a$10$Kc/0I11BVSsNDxsuYAsJDekSvfAze0t7auWjff5OF6oC/anfVdVje"
+where id = 1;
+
+
+insert into parentAuth(studentId, parentPass)
+values ("nikhil","nikhil"),
+       ("gaurav", "gaurav"),
+       ("aditri", "aditri");
+
+update auth.parentAuth
+set parentPass = "$2a$10$Kc/0I11BVSsNDxsuYAsJDekSvfAze0t7auWjff5OF6oC/anfVdVje"
+where id = 1;
